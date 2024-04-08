6d55ed87c67484bec612d7477aa24d7cf730597b
wmancaniello <wmancaniello@gmail.com>
2024-04-08 18:07:27 +0200

    Add Function to combinate with scripts

---
+++ README.md
@@ -0,0 +1,27 @@
+###  CONSEGNA
+
+- La calcolatrice deve fare dei calcoli tra due numeri interi. 
+- Quindi in sequenza:
+    - 1) Inserire le cifre del primo numero;
+    - 2) Selezionare l'operazione;
+    - 3) Inserire le cifre del secondo numero;
+    - 4) Cliccare su tasto `=` per ottenere il risultato.
+- **Attenzione:** Non si può applicare più di un'operazione alla volta.
+
+MILESTONE 1
+aggiungere event listener a tutti i numeri (0-9)
+quando si clicca su un numero, concatenarlo al numero visualizzato in alto
+
+MILESTONE 2
+aggiungere event listener a tutti gli operatori
+quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
+
+MILESTONE 3
+aggiungere event listener al pulsante =
+quando si clicca su =, salvare il secondo operando, effetuare il calcolo corretto in base all'operatore selezionato e visualizzare il risultato in alto
+
+MILESTONE 4
+pulsante canc: cliccando il pulsante si resetta il calcolo
+:stella2: BONUS
+verificare che non si stia cercando di dividere per 0
+
---
+++ js/function.js
@@ -0,0 +1,61 @@
+/**
+ * Descrizione:  Funzione che controlla e gestisce la presenza dei numeri sul display.
+ * @param {string} number Il numero da mostrare sul display.
+ */
+function displayNumber(number) {
+  //  seleziono il display
+  const display = document.getElementById("result");
+
+  //  Check del display, se c'è già lo 0
+  if (display === "0") {
+    document.getElementById("result").innerText = number;
+  } else {
+    //  Altrimenti procedo con la concatenazione dei numeri
+    document.getElementById("result").innerText += number;
+  }
+}
+
+/**
+ * Descrizione:  Funzione per il calcolo matematico tra due numeri. (Addizione, Sottrazione, Moltiplicazione, Divisione)
+ * @param {string} sign ,simbolo operazione
+ * @param {number} num1 ,primo numero
+ * @param {number} num2 ,secondo numero
+ * @returns {number/string} se è errore sarà string, altrimenti number
+ */
+function operations(sign, num1, num2) {
+  let result;
+
+  //  Addizione
+  if (sign === "+") {
+    result = num1 + num2;
+  }
+  //  Sottrazione
+  else if (sign === "-") {
+    result = num1 - num2;
+  }
+  //  Moltiplicazione
+  else if (sign === "&#215;") {
+    result = num1 * num2;
+  }
+  //  Divisione
+  else if (sign === "&#247;") {
+    if (num2 === 0) {
+      result = "ERROR";
+    } else {
+      result = num1 / num2;
+    }
+  }
+  //  ERROR
+  else {
+    result = "error";
+  }
+
+  return result;
+}
+
+/**
+ * Funzione che resetti il display della calcolatrice.
+ */
+function displayBack() {
+  document.getElementById("result").innerText = "0";
+}
